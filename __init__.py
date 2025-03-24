f00ee934b415b8fbdd2a8e231d73597ee3ed47c7
xie236393 <2363939182@qq.com>
2025-03-24 20:24:39 +0800

    Fix __init__.py formatting

--- __init__.py
+++ __init__.py
@@ -1,11 +1,13 @@
-from .nodes import SiliconFlowNode
+from .nodes import SiliconFlowNode, SiliconFlowVideoNode
 
 NODE_CLASS_MAPPINGS = {
-    "SiliconFlowNode": SiliconFlowNode
+    "SiliconFlowNode": SiliconFlowNode,
+    "SiliconFlowVideoNode": SiliconFlowVideoNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SiliconFlowNode": "硅基流动2U"
+    "SiliconFlowNode": "硅基流动2U",
+    "SiliconFlowVideoNode": "硅基流动视频生成API"
 }
 
 __version__ = "1.0.0"
